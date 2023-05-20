d722fe79a414b603679208a59c2c635f71c73bb2
mizza-cz <mizza.cz666@gmail.com>
2023-05-20 14:17:21 +0200

    add hp

---
+++ src/components/topstory/topstory.js
@@ -0,0 +1,11 @@
+$(".topstory__inner").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: true,
+  infinite: true,
+  prevArrow:
+    '<button class="slider__btn slider__btnprev"><img src="../images/ico/left.svg" alt="" ></button> ',
+  nextArrow:
+    ' <button class="slider__btn  slider__btnnext"><img src="../images/ico/right.svg" alt = "" ></button>',
+});
